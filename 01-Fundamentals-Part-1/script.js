1a97cb039d9828923495171067668d3e4cfcd21a
maxamediidg <maxamedeid72@gmail.com>
2024-02-10 22:01:38 +0300

    commit🚀🚀

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,33 @@
-      let js = 'amazing'
-      if(js === 'amazing')alert('javascript is Fun')
+
+let js = 'amazing'
       console.log(40+50);
+
+
+console.log("jonas");
+console.log(23);
+
+//Varibles
+let firstName ="jonas";
+console.log(firstName);
+
+let myfirst_job = 'programmer';
+console.log(myfirst_job);
+
+let $new = 28;
+
+//Data Types
+let javascriptIsFn = true;
+console.log(javascriptIsFn)
+console.log(typeof javascriptIsFn);
+console.log(typeof 123);
+console.log(typeof 'jonas');
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2024;
+console.log(year);
+console.log(typeof year);
+
+
